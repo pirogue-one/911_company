48f1dd421cb7ad9cabea6daf024f53e9f2c4ee2f
pirogue-one <alyona.evg@yandex.ru>
2023-12-13 21:35:13 +0300

    added some edits to destktop version

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
-// mouse 
+//mouse move
 
-// let mouseDown = false;
-// let startX, scrollLeft;
-// const slider = document.querySelectorAll('.news');
+let mouseDown = false;
+let startX, scrollLeft;
+const slider = document.querySelector('.hero');
 
-// const startDragging = (e) => {
-//     mouseDown = true;
-//     startX = e.pageX - slider.offsetLeft;
-//     scrollLeft = slider.scrollLeft;
-// }
+const startDragging = (e) => {
+    mouseDown = true;
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+}
 
-// const stopDragging = (e) => {
-//     mouseDown = false;
-// }
+const stopDragging = (e) => {
+    mouseDown = false;
+}
 
-// const move = (e) => {
-//     e.preventDefault();
-//     if (!mouseDown) { return; }
-//     const x = e.pageX - slider.offsetLeft;
-//     const scroll = x - startX;
-//     slider.scrollLeft = scrollLeft - scroll;
-// }
+const move = (e) => {
+    e.preventDefault();
+    if (!mouseDown) { return; }
+    const x = e.pageX - slider.offsetLeft;
+    const scroll = x - startX;
+    slider.scrollLeft = scrollLeft - scroll;
+}
+
+// Add the event listeners
+slider.addEventListener('mousemove', move, false);
+slider.addEventListener('mousedown', startDragging, false);
+slider.addEventListener('mouseup', stopDragging, false);
+slider.addEventListener('mouseleave', stopDragging, false);
